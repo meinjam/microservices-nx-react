645ca7083f8f7a953c3b291c014ce5567fbd8660
meinjam <injam.cse@gmail.com>
2022-09-27 22:37:40 +0600

    two react app add done

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -2,6 +2,8 @@
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
 
+import { Route, Routes, Link } from 'react-router-dom';
+
 export function App() {
   return (
     <>
